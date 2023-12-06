e5eb4e75900b357468bce37ee6e1c9353c7c7b51
hall2020 <halilarbaz@gmail.com>
2023-12-05 20:07:27 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello Batch 18");
+        System.out.println("Hi");
 
         }
     }
